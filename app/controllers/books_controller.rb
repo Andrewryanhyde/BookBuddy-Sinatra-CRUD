9329a3ca2896aebcfeafa393466f36b300c871d2
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-17 10:52:03 +0000

    automatically backed up by learn

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,9 +26,6 @@ class BooksController < ApplicationController
       redirect :'/books/new'
     else
       @book = Book.create(params)
-      @book[title] = params[:title]
-      @book.author = params[:author]
-      @book.blurb = params[:blurb]
       @book.user_id = session[:user_id]
       @book.save
 
